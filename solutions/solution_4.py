9710db1fcd4338e4a00bd8bcd97a373b6c5991a7
Thijs Vogels <thijs.vogels@epfl.ch>
2020-01-24 20:11:57 +0100

    thorough pass over notebook 4, including solutions

--- solutions/solution_4.py
+++ solutions/solution_4.py
@@ -1,16 +1,42 @@
-import torch.nn as nn
-import torch.nn.functional as F
+### Examples in the MNIST dataset
 
-class MLP(nn.Module):
-    def __init__(self, in_size, h1_dim, h2_dim, num_classes):
-        super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_size, h1_dim)
-        self.fc2 = nn.Linear(h1_dim, h2_dim)
-        self.fc3 = nn.Linear(h2_dim, num_classes)
-    
+n_images = len(train_dataset)
+print(f"There are {n_images} images in the train dataset.")
+
+first_datapoint = train_dataset[0]
+print(f"Data points are a {type(first_datapoint)} of length {len(first_datapoint)}.")
+print("The entries are the input and the correct class (the digit.)")
+
+X, y = first_datapoint
+print(f"The images are {type(X)}'s of shape {X.shape}.")
+
+
+### Multi-threading in dataloaders
+
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=1
+)
+
+
+### Neural network implementation
+
+import torch.nn.functional as F  # provides some helper functions like Relu's, Sigmoids, Tanh, etc.
+
+
+class MyNeuralNetwork(torch.nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(MyNeuralNetwork, self).__init__()
+
+        self.input_size = input_size
+        self.num_classes = num_classes
+
+        self.linear_1 = torch.nn.Linear(input_size, 75)
+        self.linear_2 = torch.nn.Linear(75, 50)
+        self.linear_3 = torch.nn.Linear(50, num_classes)
+
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        return F.softmax(x)
-    
+        out = F.relu(self.linear_1(x))
+        out = F.relu(self.linear_2(out))
+        out = self.linear_3(out)
+        return out
+
